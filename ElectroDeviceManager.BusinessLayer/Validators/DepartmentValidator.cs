098d61b0c42e38762387df6a67615b60c2b302cf
Doğukan Bayazit <dogukanbyzt@gmail.com>
2024-12-03 17:26:38 +0300

    Her entity için validatorlar eklendi

---
+++ ElectroDeviceManager.BusinessLayer/Validators/DepartmentValidator.cs
@@ -0,0 +1,20 @@
+﻿using ElectronicDeviceManager.EntityLayer.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicDeviceManager.BusinessLayer.Validators
+{
+    public class DepartmentValidator : AbstractValidator<Department>
+    {
+        public DepartmentValidator()
+        {
+            RuleFor(d => d.Name).NotEmpty().WithMessage("Departman Adı Boş Geçilemez");
+            RuleFor(d => d.Description).NotEmpty().WithMessage("Açıklama Kısımı Boş Geçilemez");
+
+        }
+    }
+}
---
+++ ElectroDeviceManager.BusinessLayer/Validators/EmployeeValidator.cs
@@ -0,0 +1,20 @@
+﻿using ElectronicDeviceManager.EntityLayer.Models;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicDeviceManager.BusinessLayer.Validators
+{
+    public class EmployeeValidator : AbstractValidator<Employee>
+    {
+        public EmployeeValidator() { 
+        RuleFor(e=> e.FirstName).NotEmpty().WithMessage("Bu alan boş geçilemez");
+        RuleFor(e=> e.LastName).NotEmpty().WithMessage("Bu alan boş geçilemez");
+        RuleForEach(e=> e.Position).NotEmpty().WithMessage("Bu alan boş geçilemez");
+        
+        }
+    }
+}
